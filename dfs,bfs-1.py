1952670abef130d6793f1d60dfaed8f83d55cb49
honey989 <78430657+honey989@users.noreply.github.com>
2022-10-14 17:02:58 +0900

    Add files via upload

---
+++ dfs,bfs-1.py
@@ -0,0 +1,32 @@
+# LV2. 깊이/너비 우선 탐색(DFS/BFS) - 타겟 넘버
+# bfs
+def solution(numbers, target):
+    answer = 0
+    queue = [[numbers[0],0], [-1*numbers[0],0]]
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+
+#dfs
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
