e8d50becf15ff30d6f6d08495f162fc147974961
honey989 <78430657+honey989@users.noreply.github.com>
2022-10-14 17:07:12 +0900

    Update dfs,bfs-1.py

--- dfs,bfs-1.py
+++ dfs,bfs-1.py
@@ -30,3 +30,5 @@ def solution(numbers, target):
             dfs(idx+1, result-numbers[idx])
     dfs(0,0)
     return answer
+
+# 출처: https://velog.io/@ju_h2/Python-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-level2-%ED%83%80%EA%B2%9F%EB%84%98%EB%B2%84-BFSDFS
